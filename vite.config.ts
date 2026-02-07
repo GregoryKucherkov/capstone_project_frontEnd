fe29e2f5fe6130d0cda7fddd813e644dad359d31
Gregory Kucherkov <gregorykucherkov@Mac.N4>
2026-02-07 09:30:45 +0200

    hero fix7

--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' 
+      }
+    },
+    devSourcemap: true,
+  }
 });
